399309ab9d3627bb1eab1ed5c36236fe3573b085
Leonid <jiehuh87@yandex.ru>
2021-06-16 23:34:15 +0300

    Добавлена форма для отображения информации для игроков Добавлены команды для увеличения счета у игроков

--- GuessMelody/App.xaml.cs
+++ GuessMelody/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GuessMelody.View;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,9 @@ namespace GuessMelody
     /// </summary>
     public partial class App : Application
     {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            new UserWindow().Show();
+        }
     }
 }
--- GuessMelody/ViewModel/ViewModel.cs
+++ GuessMelody/ViewModel/ViewModel.cs
@@ -1,12 +1,167 @@
-﻿using System;
+﻿using GuessMelody.View;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace GuessMelody.ViewModel
 {
-    class ViewModel
+    class ViewModel : INotifyPropertyChanged
     {
+        int _scorePlayer1 = 0;
+        int _scorePlayer2 = 0;
+        int _scorePlayer3 = 0;
+        int _scorePlayer4 = 0;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public int ScorePlayer1
+        {
+            get => _scorePlayer1;
+            set
+            {
+                _scorePlayer1 = value;
+                OnPropertyChanged("ScorePlayer1");
+            }
+        }
+        public int ScorePlayer2
+        {
+            get => _scorePlayer2;
+            set
+            {
+                _scorePlayer2 = value;
+                OnPropertyChanged("ScorePlayer2");
+            }
+        }
+        public int ScorePlayer3
+        {
+            get => _scorePlayer3;
+            set
+            {
+                _scorePlayer3 = value;
+                OnPropertyChanged("ScorePlayer3");
+            }
+        }
+        public int ScorePlayer4
+        {
+            get => _scorePlayer4;
+            set
+            {
+                _scorePlayer4 = value;
+                OnPropertyChanged("ScorePlayer4");
+            }
+        }
+
+        public ICommand OpenSetting
+        {
+            get
+            {
+                return new DelegateCommand((p) =>
+                {
+                    Debug.WriteLine("Test");
+                    Settings settings = new Settings();
+                    settings.ShowDialog();
+                }, (p) => true);
+            }
+        }
+        public ICommand LeftClickCommandPl1
+        {
+            get
+            {
+                return new DelegateCommand((p) =>
+                {
+                    Debug.WriteLine("Test++");
+                    ScorePlayer1++;
+                }, (p) => true);
+            }
+        }
+        public ICommand RightClickCommandPl1
+        {
+            get
+            {
+                return new DelegateCommand((p) =>
+                {
+                    Debug.WriteLine("Test--");
+                    ScorePlayer1--;
+                }, (p) => true);
+            }
+        }
+        public ICommand LeftClickCommandPl2
+        {
+            get
+            {
+                return new DelegateCommand((p) =>
+                {
+                    Debug.WriteLine("Test++");
+                    ScorePlayer2++;
+                }, (p) => true);
+            }
+        }
+        public ICommand RightClickCommandPl2
+        {
+            get
+            {
+                return new DelegateCommand((p) =>
+                {
+                    Debug.WriteLine("Test--");
+                    ScorePlayer2--;
+                }, (p) => true);
+            }
+        }
+        public ICommand LeftClickCommandPl3
+        {
+            get
+            {
+                return new DelegateCommand((p) =>
+                {
+                    Debug.WriteLine("Test++");
+                    ScorePlayer3++;
+                }, (p) => true);
+            }
+        }
+        public ICommand RightClickCommandPl3
+        {
+            get
+            {
+                return new DelegateCommand((p) =>
+                {
+                    Debug.WriteLine("Test--");
+                    ScorePlayer3--;
+                }, (p) => true);
+            }
+        }
+        public ICommand LeftClickCommandPl4
+        {
+            get
+            {
+                return new DelegateCommand((p) =>
+                {
+                    Debug.WriteLine("Test++");
+                    ScorePlayer4++;
+                }, (p) => true);
+            }
+        }
+        public ICommand RightClickCommandPl4
+        {
+            get
+            {
+                return new DelegateCommand((p) =>
+                {
+                    Debug.WriteLine("Test--");
+                    ScorePlayer4--;
+                }, (p) => true);
+            }
+        }
     }
 }
