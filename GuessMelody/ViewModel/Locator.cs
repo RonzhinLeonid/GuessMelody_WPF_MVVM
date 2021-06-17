8f73a415c1a5b6282f3664bb266de7a0a7efb58a
Андрей Заярный <zaazaa@yandex.ru>
2021-06-17 19:06:11 +0300

    Две вьюшки подключены к одной вьюмодели

---
+++ GuessMelody/ViewModel/Locator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessMelody.ViewModel
+{
+    class Locator
+    {
+       public GuessMelody.ViewModel.ViewModel MainViewModel { get; } = new ViewModel();
+
+    }
+}
